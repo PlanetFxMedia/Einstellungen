cc61e88c12f4642d9d8659cb825e6836b011ff6b
swtw122 <sebastian.mikolai@gmail.com>
2016-08-16 23:39:38 +0200

    Try to Fix MySQL Dump v1.1.2

--- src/de/SebastianMikolai/PlanetFx/Einstellungen/Datenbank/MySQL.java
+++ src/de/SebastianMikolai/PlanetFx/Einstellungen/Datenbank/MySQL.java
@@ -10,27 +10,32 @@ import java.util.UUID;
 import de.SebastianMikolai.PlanetFx.Einstellungen.Einstellungen;
 
 public class MySQL {
-
-	public static Connection c = null;
-	public static Statement database;
-	public static Connection c_uuids = null;
-	public static Statement database_uuids;
 	
-	public static void Connect() {
+	public static Connection Connect() {
 		try {
-			c = DriverManager.getConnection("jdbc:mysql://" + Einstellungen.getInstance().getConfig().getString("database.host") + ":" + 
+			Connection con = DriverManager.getConnection("jdbc:mysql://" + Einstellungen.getInstance().getConfig().getString("database.host") + ":" + 
 					Einstellungen.getInstance().getConfig().getInt("database.port") + "/" + Einstellungen.getInstance().getConfig().getString("database.db") + 
 					"?user=" + Einstellungen.getInstance().getConfig().getString("database.user") + "&password=" + Einstellungen.getInstance().getConfig().getString("database.password"));
-			database = c.createStatement();
 			Einstellungen.getInstance().getLogger().info("Die Verbindung zur Datenbank wurde hergestellt!");
+			return con;
 		} catch (Exception e) {
-			Einstellungen.getInstance().getPluginLoader().disablePlugin(Einstellungen.getInstance());
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static void Close(Connection con) {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	public static void LadeTabellen() {
 		try {
-			Statement stmt = c.createStatement();
+			Connection con = Connect();
+			Statement stmt = con.createStatement();
 			ResultSet rss = stmt.executeQuery("SHOW TABLES LIKE 'EinstellungenPlayerConfig'");
 			if (rss.next()) {
 				Einstellungen.getInstance().getLogger().info("Die Tabelle EinstellungenPlayerConfig wurde geladen!");
@@ -38,34 +43,55 @@ public class MySQL {
 				int rs = stmt.executeUpdate("CREATE TABLE EinstellungenPlayerConfig (id INTEGER PRIMARY KEY AUTO_INCREMENT, uuid TEXT, name TEXT, partikel INTEGER)");
 				Einstellungen.getInstance().getLogger().info("Die Tabelle EinstellungenPlayerConfig wurde erstellt! (" + rs + ")");
 			}
+			Close(con);
 		} catch (SQLException e) {
+			e.printStackTrace();
 			Einstellungen.getInstance().getPluginLoader().disablePlugin(Einstellungen.getInstance());
 		}
 	}
 	
 	public static PlayerKonfiguration getPlayerConfig(UUID uuid) {
-		PlayerKonfiguration pk = null;
 		try {
-			Statement stmt = c.createStatement();
-			ResultSet rss = stmt.executeQuery("SELECT * FROM EinstellungenPlayerConfig WHERE uuid='" + uuid + "'");
-			while (rss.next()) {
-				pk = new PlayerKonfiguration(uuid, rss.getString("name"), rss.getInt("partikel"));
+			PlayerKonfiguration pk = null;
+			Connection con = Connect();
+			if (con != null) {
+				Statement stmt = con.createStatement();
+				ResultSet rss = stmt.executeQuery("SELECT * FROM EinstellungenPlayerConfig WHERE uuid='" + uuid + "'");
+				while (rss.next()) {
+					pk = new PlayerKonfiguration(uuid, rss.getString("name"), rss.getInt("partikel"));
+				}
 			}
-		} catch (SQLException e) {}
-		return pk;
+			Close(con);
+			return pk;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public static void addPlayerConfig(PlayerKonfiguration pk) {
 		try {
-			Statement stmt = c.createStatement();
-			stmt.execute("INSERT INTO EinstellungenPlayerConfig (uuid, name, hideplayer, scoreboard, partikel) VALUES ('" + pk.getUUID() + "', '" + pk.name + "', '" + pk.partikel + "')");
-		} catch (SQLException e) {}
+			Connection con = Connect();
+			if (con != null) {
+				Statement stmt = con.createStatement();
+				stmt.execute("INSERT INTO EinstellungenPlayerConfig (uuid, name, hideplayer, scoreboard, partikel) VALUES ('" + pk.getUUID() + "', '" + pk.name + "', '" + pk.partikel + "')");
+			}
+			Close(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void updatePlayerConfig(PlayerKonfiguration pk) {
 		try {
-			Statement stmt = c.createStatement();
-			stmt.executeUpdate("UPDATE EinstellungenPlayerConfig SET name='" + pk.getName() + "', partikel='" + pk.partikel + "' WHERE uuid='" + pk.getUUID() + "'");
-		} catch (SQLException e) {}
+			Connection con = Connect();
+			if (con != null) {
+				Statement stmt = con.createStatement();
+				stmt.executeUpdate("UPDATE EinstellungenPlayerConfig SET name='" + pk.getName() + "', partikel='" + pk.partikel + "' WHERE uuid='" + pk.getUUID() + "'");
+			}
+			Close(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
