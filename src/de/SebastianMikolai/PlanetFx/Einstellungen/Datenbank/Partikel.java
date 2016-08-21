0071e1fef3f5f2f1d49d7bda2b7c332df7bb9a8b
swtw122 <sebastian.mikolai@gmail.com>
2016-08-22 01:35:43 +0200

    Version v1.1.3

---
+++ src/de/SebastianMikolai/PlanetFx/Einstellungen/Datenbank/Partikel.java
@@ -0,0 +1,22 @@
+package de.SebastianMikolai.PlanetFx.Einstellungen.Datenbank;
+
+public enum Partikel {
+	happyvillager,
+	reddust,
+	witchmagic,
+	flame,
+	rainbow,
+	enchantmenttable,
+	explode,
+	fireworksspark,
+	smoke,
+	damageindicator,
+	endrod,
+	dragonbreath,
+	heart,
+	cloud,
+	lava,
+	portal,
+	note,
+	snowshovel
+}
